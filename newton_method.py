c90b5c4b7fcf8d9b5898ca512db9b9a4ecb049cb
Thomas Greaux <thomas.greaux@etu.unice.fr>
2019-02-10 18:55:45 +0100

    This version should be close to the final one

--- newton_method.py
+++ newton_method.py
@@ -5,13 +5,13 @@ from mpl_toolkits import mplot3d
 import conjugate_gradient as cg
 import function as func
 
-eps = 0.001 #epsilon, the precision demanded
+eps = 0.01 #epsilon, the precision demanded
 
 def norm(x):
     return np.inner(x, x)
 
 def alpha(k):
-    return 0.02
+    return 0.001
 
 def inexact_newton(w, i, debug):
     '''
@@ -29,10 +29,10 @@ def inexact_newton(w, i, debug):
         k = k+1
     return k
 
-for d in range(2, 20):
+for d in range(2, 22):
     func.set_dimension(d)
     x0 = func.generate_x0()
-    for i in range(3, d+1):
+    for i in range(2, d+1):
         steps = inexact_newton(x0, i, 0)
         print("#steps at dimension " + str(d) + " and conjugate " + str(i) + " = " + str(steps))
     print("")
