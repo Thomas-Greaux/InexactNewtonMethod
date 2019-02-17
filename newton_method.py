e82c6b8c6fab2bdce1988b50a81f96b6a8c00507
Thomas Greaux <thomas.greaux@etu.unice.fr>
2019-02-17 18:35:44 +0100

    Implemented arguments

--- newton_method.py
+++ newton_method.py
@@ -15,7 +15,24 @@ from mpl_toolkits import mplot3d
 import conjugate_gradient as cg
 import function as func
 
-eps = 0.01 #epsilon, the precision demanded
+'''
+This block is the parsing of arguments
+'''
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Execute the inexact newton method using the conjugate gradient method")
+parser.add_argument("--maxd", type=int, metavar="", default=18, help="The maximum dimension of the function")
+parser.add_argument("--increment", type=int, metavar="", default=2, help="The increment of the number of steps in the conjugate gradient method")
+parser.add_argument("-e", "--epsilon", type=float, metavar="", default=0.01, help="Epsilon, the precision demanded")
+parser.add_argument("-a", "--alpha", type=float, metavar="", default=0.005, help="Alpha, the learning rate")
+
+args = parser.parse_args()
+
+max_d = args.maxd
+increment = args.increment
+epsilon = args.epsilon
+alpha = args.alpha
 
 def norm(x):
     '''
@@ -23,32 +40,34 @@ def norm(x):
     '''
     return np.inner(x, x)
 
-def alpha(k):
-    '''
-    returns the hyper parameter alpha, the learning rate
-    '''
-    return 0.005
-
 def inexact_newton(w, i):
     '''
     execute the inexact newton method using the conjugate gradient
     returns the number of steps necessary to achieve the requiered precision
     '''
     k = 0
-    while(func.f(w) >= eps):
+    while(func.f(w) >= epsilon):
         # This is the heart of the inexact newton
         # We follow the formula wk+1 = wk - alphak * gradient(wk) / hessian(wk)
         # But we use the conjugate gradient method that gives use " - gradient(wk) / hessian(wk) "
-        w = w + alpha(k) * cg.conjugate_gradient(w, func.hessian(w), -func.gradient(w), i)
+        w = w + alpha * cg.conjugate_gradient(w, func.hessian(w), -func.gradient(w), i)
         k = k+1
     return k
+
+'''
+This block is welcoming the user
+'''
+print("Executing with arguments : ")
+print("max d = " + str(max_d))
+print("increment = " + str(increment))
+print("epsilon = " + str(epsilon))
+print("alpha = " + str(alpha))
+print("")
 
 '''
 This block is the program,
 is increases the dimension and number of steps for the conjugate gradient.
 '''
-max_d = 18
-increment = 2
 for d in range(2, max_d+1, increment):
     func.set_dimension(d)
     x0 = func.generate_x0()
